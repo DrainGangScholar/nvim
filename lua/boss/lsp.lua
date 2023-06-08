6d37095085b60ac414840eaee98d9dddcb904081
Veljko Markovic <veljkomarkovic@Veljkos-Air.lan>
2023-06-08 23:42:30 +0200

    everything works :DDD

--- lua/boss/lsp.lua
+++ lua/boss/lsp.lua
@@ -5,23 +5,23 @@ lsp.preset("recommended")
 lsp.ensure_installed({
   'rust_analyzer',
   'clangd',
+  'csharp-ls'
 })
+
+require'lspconfig'.csharp_ls.setup{}
 
 -- Fix Undefined global 'vim'
 lsp.nvim_workspace()
 
-
+-- DORADI
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+  ["<C-1>"] = cmp.mapping.select_prev_item(cmp_select),
+  ["<C-2>"] = cmp.mapping.select_next_item(cmp_select),
+  ["<C-3>"] = cmp.mapping.confirm({ select = true }),
+  ["<C-4>"] = cmp.mapping.complete(),
 })
-
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
