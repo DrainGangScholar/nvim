c8588fa9a4350d34c1ed1e227c036c602c15168f
Veljko Markovic <veljko.markovic@atessoft.rs>
2024-10-12 14:51:45 +0200

    finished

--- lua/veljko/plugins/lsp-config.lua
+++ lua/veljko/plugins/lsp-config.lua
@@ -1,17 +1,30 @@
 return {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        { "folke/neodev.nvim", opts = {} },
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
     },
-    config = function()
-        local nvim_lsp = require("lspconfig")
-        local mason_lspconfig = require("mason-lspconfig")
-
-        local protocol = require("vim.lsp.protocol")
-        local on_attach = (function(client, bufnr)
-            local opts = { buffer = bufnr, remap = false }
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "gopls" }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.gopls.setup({
+                capabilities = capabilities
+            })
+            local opts = {}
             vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
             vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
             vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
@@ -23,20 +36,6 @@ return {
             vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
             vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
             vim.keymap.set("n", "<leader>od", function() vim.diagnostic.open_float() end, opts)
-        end)
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-        mason_lspconfig.setup({
-            ensure_installed = { "lua_ls", "gopls", "sqls" },
-            capabilities=capabilities
-        })
-
-        mason_lspconfig.setup_handlers({
-            function(server_name)
-                nvim_lsp[server_name].setup {
-                    on_attach = on_attach,
-                    capabilities = capabilities,
-                }
-            end,
-        })
-    end,
+        end
+    }
 }
--- lua/veljko/plugins/telescope.lua
+++ lua/veljko/plugins/telescope.lua
@@ -1,11 +1,27 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.6",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		require("telescope").setup()
-		local keymap = vim.keymap
-		keymap.set("n", "<leader>pf", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-		keymap.set("n", "<leader>ps", "<cmd>Telescope live_grep<cr>", { desc = "Fuzzy find recent files" })
-	end,
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.6",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("telescope").setup()
+            local keymap = vim.keymap
+            keymap.set("n", "<leader>pf", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+            keymap.set("n", "<leader>ps", "<cmd>Telescope live_grep<cr>", { desc = "Fuzzy find recent files" })
+        end,
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim" ,
+        config = function()
+            require("telescope").setup {
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {
+                        }
+                    }
+                }
+            }
+            require("telescope").load_extension("ui-select")
+        end
+    }
 }
