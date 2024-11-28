bd7442c95084b370269b1d9a5772d3a70b0c1199
DrainGangScholar <veljko.markovic@elfak.rs>
2024-11-28 18:59:40 +0100

    changed to sakura theme

--- lua/veljko/plugins/colorscheme.lua
+++ lua/veljko/plugins/colorscheme.lua
@@ -1,10 +1,16 @@
 return {
---    "slugbyte/lackluster.nvim",
-    "catppuccin/nvim",
+    --    "slugbyte/lackluster.nvim",
+    --    "catppuccin/nvim",
+    "anAcc22/sakura.nvim",
     lazy = false,
     priority = 1000,
+    dependencies = {
+        "rktjmp/lush.nvim"
+    },
     config = function()
---        vim.cmd("colorscheme lackluster-dark")
-        vim.cmd("colorscheme catppuccin-mocha")
+        --        vim.cmd("colorscheme lackluster-dark")
+        --       vim.cmd("colorscheme catppuccin-mocha")
+        vim.opt.background = "dark"
+        vim.cmd("colorscheme sakura")
     end,
 }
